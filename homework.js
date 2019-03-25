cae2490b6508275267496fddc4ea8a830aa1399d
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-25 12:25:06 -0400

    finished wuestions 1 and 2. Answer 3 not working :(

--- homework.js
+++ homework.js
@@ -1,11 +1,18 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+const givenName = 'Addison'
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+const candies = 20
+const people = 6
+//the remained is done by doing variable % variable
+const leftover = candies % people
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+function greeting(name) {
+    console.log(`Hello, ` + name + `!`)
+}
+greeting("Chase")
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
