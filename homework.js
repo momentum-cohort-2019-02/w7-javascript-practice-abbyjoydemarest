4ccad121914e079e7ef03b04d6a905cf6cdf278c
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-25 13:26:41 -0400

    answered question 5. made function isEven to find an even number

--- homework.js
+++ homework.js
@@ -25,7 +25,12 @@ function isOdd(number) {
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-
+function isEven(number) {
+    if (number % 2 === 0)
+        return true
+    else
+        return false
+}
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
